8d20ddbd91f1c0dc799b255492a10ca927d24bac
Deepak Vig <deepakvig@gmail.com>
2017-06-13 16:33:15 +0530

    Orders section added

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -4,4 +4,10 @@ ShopifyApp.configure do |config|
   config.secret = "cb68e78faf60d40b18b2b5158e229500"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
+  config.webhooks = [
+      {topic: 'app/uninstalled', address: "#{ENV['WEBHOOK_URL']}app_uninstalled", format: 'json'},
+      {topic: 'orders/delete', address: "#{ENV['WEBHOOK_URL']}orders_delete", format: 'json'},
+      {topic: 'orders/create', address: "#{ENV['WEBHOOK_URL']}orders_create", format: 'json'},
+      {topic: 'orders/updated', address: "#{ENV['WEBHOOK_URL']}orders_updated", format: 'json'}
+    ]
 end
