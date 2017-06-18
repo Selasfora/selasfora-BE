3a20ae1142ccd43b4c060a4f477f218f6e3819e9
Deepak Vig <deepakvig@gmail.com>
2017-06-18 21:59:30 +0530

    Authenticity token removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
-  
+  include DeviseTokenAuth::Concerns::SetUserByToken
 end
