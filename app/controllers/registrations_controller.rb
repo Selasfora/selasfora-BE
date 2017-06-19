1f23d0af1bb9baa8931633813fd0cdd94b385fd1
Deepak Vig <deepakvig@gmail.com>
2017-06-19 21:31:55 +0530

    Registration updated

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,19 @@
 class RegistrationsController < Devise::RegistrationsController
+  
+  def create
+
+     respond_to do |format|
+       format.html {
+         super
+       }
+       format.json {
+         @user = User.create(sign_up_params)
+         @user.save ? (render :json => {:state => {:code => 0}, :data => @user }) : 
+                      (render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} })
+       }
+     end
+   end
+  
 
   private
 
