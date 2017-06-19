a45c7ec4756d99931ffbd584653e1fe18e81b459
Deepak Vig <deepakvig@gmail.com>
2017-06-19 08:09:49 +0530

    Custom fields added to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', :controllers => { registrations: 'registrations' }
   root :to => 'orders#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
