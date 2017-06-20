2ed00d86f3615a2fa695388a3a5507913dec22eb
Deepak Vig <deepakvig@gmail.com>
2017-06-20 08:44:28 +0530

    Email url updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://selasfora-dev.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: "selasfora-dev.herokuapp.com" }
 end
