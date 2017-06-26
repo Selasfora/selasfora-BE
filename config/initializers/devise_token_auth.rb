fe69c2d436985b85b77be773afd9608245d35ab6
Oladayo Oyelade <ayfrank2@gmail.com>
2017-06-26 16:20:42 +0100

    refactor and customize mailer

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,6 +1,7 @@
 DeviseTokenAuth.setup do |config|
 
-	config.default_confirm_success_url = "http://selasfora.surge.sh/login"
+  config.default_confirm_success_url = "http://selasfora.surge.sh/login"
+  # config.mailer_sender = "hello@selasfora.surge.sh"
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
     scope: ShopifyApp.configuration.scope
-  provider :facebook,      '1389149917845163', 'c3c48939366edcb08f20f354455bb080'
-  provider :google_oauth2, '561313107749-flfhdb086rglddft350c1c8e3aolju6c.apps.googleusercontent.com', 'z7ykGvdn3O583MN0UB0f4Cwu'
-  provider :twitter,      '36r0UcbrGLXjCFMAVi9q0tLOW', 'ygOR3tY5Vc1hXhoQ0v3Sjmded323PEAgIe1hUTX5jscRce0T3j'
+  provider :facebook,      ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_OAUTH_URL'], ENV['GOOGLE_SECRET']
+  provider :twitter,      ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
