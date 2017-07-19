af7e20b3343f8a4234db09094ad3022324f41f19
Oladayo Oyelade <ayfrank2@gmail.com>
2017-07-19 15:02:06 +0100

    refactor code

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,26 +3,26 @@ class BlogsController < ApplicationController
     page_limit = params[:limit] || 50
     page_num = params[:page] || 1
     #TODO put shop and credentials in config file
-    all_blogs = ShopifyAPI::Session.temp("selafore-staging.myshopify.com", "c3973c904c916f7865223c8e3cd754aa") { ShopifyAPI::Blog.find(:all, :params => {:limit => page_limit, :page => page_num}) }
+    all_blogs = ShopifyAPI::Session.temp(ENV['SHOPIFY_API_URL'], ENV['SHOPIFY_API_KEY']) { ShopifyAPI::Blog.find(:all, :params => {:limit => page_limit, :page => page_num}) }
     render json: all_blogs
   end
 
   def getSpecifyBlog
     blog_id = params[:id]
     #TODO put shop and credentials in config file
-    blog = ShopifyAPI::Session.temp("selafore-staging.myshopify.com", "c3973c904c916f7865223c8e3cd754aa") { ShopifyAPI::Blog.find(blog_id) }
+    blog = ShopifyAPI::Session.temp(ENV['SHOPIFY_API_URL'], ENV['SHOPIFY_API_KEY']) { ShopifyAPI::Blog.find(blog_id) }
     render json: blog
   end
 
   def getAllArticles
-    articles = ShopifyAPI::Session.temp("selafore-staging.myshopify.com", "c3973c904c916f7865223c8e3cd754aa") { ShopifyAPI::Article.find(:all) }
+    articles = ShopifyAPI::Session.temp(ENV['SHOPIFY_API_URL'], ENV['SHOPIFY_API_KEY']) { ShopifyAPI::Article.find(:all) }
     render json: articles
   end
 
   def getArticlesForBlog
     id = params[:id]
     #TODO put shop and credentials in config file
-    articles = ShopifyAPI::Session.temp("selafore-staging.myshopify.com", "c3973c904c916f7865223c8e3cd754aa") { ShopifyAPI::Article.find(:all, :params => { :blog_id => id }) }
+    articles = ShopifyAPI::Session.temp(ENV['SHOPIFY_API_URL'], ENV['SHOPIFY_API_KEY']) { ShopifyAPI::Article.find(:all, :params => { :blog_id => id }) }
     render json: articles
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,28 +3,28 @@ class ProductsController < ApplicationController
     page_limit = params[:limit] || 50
     page_num = params[:page] || 1
     #TODO put shop and credentials in config file
-    all_products = ShopifyAPI::Session.temp("selafore-staging.myshopify.com", "c3973c904c916f7865223c8e3cd754aa") { ShopifyAPI::Product.find(:all, :params => {:limit => page_limit, :page => page_num}) }
+    all_products = ShopifyAPI::Session.temp(ENV['SHOPIFY_API_URL'], ENV['SHOPIFY_API_KEY']) { ShopifyAPI::Product.find(:all, :params => {:limit => page_limit, :page => page_num}) }
     render json: all_products 
   end
 
   def getSingleProduct
     product_id = params[:id]
     #TODO put shop and credentials in config file
-    product = ShopifyAPI::Session.temp("selafore-staging.myshopify.com", "c3973c904c916f7865223c8e3cd754aa") { ShopifyAPI::Product.find(product_id) }
+    product = ShopifyAPI::Session.temp(ENV['SHOPIFY_API_URL'], ENV['SHOPIFY_API_KEY']) { ShopifyAPI::Product.find(product_id) }
     render json: product 
   end
 
   def getAllCharmProducts
     page_limit = params[:limit] || 50
     page_num = params[:page] || 1
-    charm_products = ShopifyAPI::Session.temp("selafore-staging.myshopify.com", "c3973c904c916f7865223c8e3cd754aa") { ShopifyAPI::Product.where(:product_type => "charm") }
+    charm_products = ShopifyAPI::Session.temp(ENV['SHOPIFY_API_URL'], ENV['SHOPIFY_API_KEY']) { ShopifyAPI::Product.where(:product_type => "charm") }
     render json: charm_products
   end
 
   def getAllBraceletProducts
     page_limit = params[:limit] || 50
     page_num = params[:page] || 1
-    charm_products = ShopifyAPI::Session.temp("selafore-staging.myshopify.com", "c3973c904c916f7865223c8e3cd754aa") { ShopifyAPI::Product.where(:product_type => "bracelet") }
+    charm_products = ShopifyAPI::Session.temp(ENV['SHOPIFY_API_URL'], ENV['SHOPIFY_API_KEY']) { ShopifyAPI::Product.where(:product_type => "bracelet") }
     render json: charm_products
   end
 
