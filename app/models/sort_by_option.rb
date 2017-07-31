5734e6e206527cb9be3191e78b5dc4f66d742f16
Oladayo Oyelade <ayfrank2@gmail.com>
2017-07-31 23:13:26 +0100

    add migration file

---
+++ app/models/sort_by_option.rb
@@ -0,0 +1,2 @@
+class SortByOption < ApplicationRecord
+end
---
+++ test/models/sort_by_option_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SortByOptionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
