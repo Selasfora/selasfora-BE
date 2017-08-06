37c0568f29bac98221e04a5e0cfb812dabbc9f34
Oladayo Oyelade <ayfrank2@gmail.com>
2017-08-06 20:20:26 +0100

    modify access

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "selafora-dev"
   config.api_key = "3abd3e98ba88164a0b4e84acd1d16760"
   config.secret = "315ed00fc2dd34a5b794ea39915ed591"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders,read_products,read_content,read_customers, write_customers"
   config.embedded_app = true
   config.webhooks = [
       {topic: 'app/uninstalled', address: "#{ENV['WEBHOOK_URL']}app_uninstalled", format: 'json'},
